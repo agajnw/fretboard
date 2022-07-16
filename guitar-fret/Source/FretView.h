0bbdf3552bbaf2d29902569aefd42d9ca5e2b7ad
Aga Janowicz <agjanowicz@gmail.com>
2022-07-16 12:17:01 +0100

    Style with colours

--- guitar-fret/Source/FretView.h
+++ guitar-fret/Source/FretView.h
@@ -10,23 +10,38 @@
 
 #pragma once
 
+struct NoteData
+{
+    juce::String name;
+    juce::Colour colour;
+    size_t index{};
+};
+
 struct FretView  : public juce::Component
 {
-    FretView (const juce::String& name = {})
-        : label (name, name)
+    FretView (const NoteData& d = {})
+        : label (d.name, d.name),
+          data (d)
     {
         label.setJustificationType (juce::Justification::centred);
         label.setColour (juce::Label::ColourIds::textColourId,
-                         juce::Colours::darkslategrey);
+                         juce::Colours::darkgrey);
 
         addChildComponent (label);
-        label.setVisible (name.isNotEmpty());
+        label.setVisible (data.name.isNotEmpty());
     }
 
-    void setNoteName (const juce::String& name)
+    void setData (const NoteData& d)
     {
-        label.setText (name, juce::dontSendNotification);
-        label.setVisible (name.isNotEmpty());
+        data = d;
+
+        label.setText (data.name, juce::dontSendNotification);
+        label.setVisible (data.name.isNotEmpty());
+    }
+
+    void setBgColour (juce::Colour col)
+    {
+        bgColour = col;
     }
 
     void resized() override
@@ -45,7 +60,7 @@ struct FretView  : public juce::Component
         };
 
         if (bgColour.has_value())
-            g.fillAll (*bgColour);
+            g.fillAll (bgColour->withAlpha (0.2f));
 
         if (! label.isVisible())
         {
@@ -54,9 +69,10 @@ struct FretView  : public juce::Component
         }
         else
         {
-            const auto circleB = b.withSizeKeepingCentre (circleSize, circleSize);
+            const auto circleB = b.withSizeKeepingCentre (circleSize,
+                                                          circleSize);
 
-            g.setColour (circleColour);
+            g.setColour (data.colour.interpolatedWith (juce::Colours::white, 0.6f));
             g.fillEllipse (circleB.toFloat());
 
             g.setColour (lineColour);
@@ -67,13 +83,15 @@ struct FretView  : public juce::Component
         g.fillRect (b.getRight() - 1, b.getY(), 1, b.getHeight());
     }
 
+    static constexpr auto circleSize = 35;
+
+private:
     juce::Label label;
 
-    juce::Colour circleColour = juce::Colours::lightcyan;
+    NoteData data;
+
     juce::Colour lineColour = juce::Colours::white;
     std::optional<juce::Colour> bgColour;
-
-    static constexpr auto circleSize = 35;
 };
 
 struct StringView  : public juce::Component
@@ -85,18 +103,23 @@ struct StringView  : public juce::Component
 
         addAndMakeVisible (openNoteName);
 
-        auto noteIndex = (int) std::distance (noteNames.begin(),
-                                              std::find (noteNames.begin(),
-                                                         noteNames.end(),
-                                                         noteNameIn));
+        auto noteIndex = (int) std::distance (noteData.begin(),
+                                              std::find_if (noteData.begin(),
+                                                            noteData.end(),
+                                                            [&noteNameIn] (const auto& n)
+                                                            { return n.name == noteNameIn; }));
 
         for (auto& f : frets)
         {
-            noteIndex = ++noteIndex % noteNames.size();
-            f.setNoteName (noteNames [noteIndex]);
+            noteIndex = ++noteIndex % noteData.size();
+
+            f.setData (noteData[noteIndex]);
 
             addAndMakeVisible (f);
         }
+
+        for (auto i : { 3, 5, 7, 9, 12 })
+            frets[i - 1].setBgColour (fretHighlight);
     }
 
     void resized() override
@@ -123,10 +146,19 @@ struct StringView  : public juce::Component
         g.fillRect (b.getX() + FretView::circleSize - 1, b.getY(), 2, b.getHeight());
     }
 
+private:
     static constexpr auto numFrets = 12;
+    static constexpr auto numNaturals = 7;
 
-    std::array<juce::String, 12> noteNames { "C", "C#", "D", "D#", "E", "F",
-                                             "F#", "G", "G#", "A", "A#", "B" };
+    std::array<NoteData, numNaturals> noteData {{ { "C", juce::Colours::crimson, 0 },
+                                                  { "D", juce::Colours::cyan, 1 },
+                                                  { "E", juce::Colours::green, 2 },
+                                                  { "F", juce::Colours::magenta, 3 },
+                                                  { "G", juce::Colours::orange, 4 },
+                                                  { "A", juce::Colours::yellowgreen, 5 },
+                                                  { "B", juce::Colours::blueviolet, 6 } }};
+
+    juce::Colour fretHighlight = juce::Colours::white;
 
     std::array<FretView, numFrets> frets;
     juce::Label openNoteName;
@@ -151,6 +183,12 @@ struct FretboardView  : public juce::Component
         box.performLayout (getLocalBounds());
     }
 
+    void paint (juce::Graphics& g) override
+    {
+        g.fillAll (juce::Colours::darkgrey);
+    }
+
+private:
     static constexpr auto numStrings = 6;
     std::array<StringView, numStrings> strings { juce::String ("E"),
                                                  { "A" },
